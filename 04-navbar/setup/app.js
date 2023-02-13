cb050b46b767a2e6ea77d987dbe6b4e008e3134b
peacekeeps <app.juliog@gmail.com>
2023-02-13 08:54:07 -0300

    navbar completed

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,18 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle")
+const links = document.getElementById("links")
+
+console.log(navToggle, links)
+
+navToggle.addEventListener("click", function() {
+    console.log(links.classList)
+    // if (links.classList.contains("show-menu")) {
+    //     links.classList.remove("show-menu")
+    // } else {
+    //     links.classList.add("show-menu")
+    // }
+    links.classList.toggle("show-menu")
+})
